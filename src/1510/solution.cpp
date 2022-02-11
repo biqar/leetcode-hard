3b3949afbe2486b3d76de936d50d70d94c992c60
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2022-02-10 21:01:21 -0500

    Add problem statement and solution of p-1510

--- src/1510/solution.cpp
+++ src/1510/solution.cpp
@@ -1,4 +1,25 @@
 //
-// Created by Islam, Abdullah Al Raqibul on 02/04/22.
+// Created by Islam, Abdullah Al Raqibul on 02/10/22.
 //
 
+class Solution {
+public:
+  bool recur(int n, vector<int> &dp) {
+    if(n <= 0) return false;
+    if(dp[n] != -1) return dp[n];
+
+    for(int i=1; i*i<=n; i+=1) {
+      int ret = recur(n-(i*i), dp);
+      if(!ret) return dp[n] = 1;
+    }
+    return dp[n] = 0;
+  }
+
+  bool winnerSquareGame(int n) {
+    vector<int> dp(max(n+1,3), -1);
+    dp[1] = 1;
+    dp[2] = 0;
+
+    return recur(n, dp);
+  }
+};
