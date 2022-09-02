6d2fe554462811160350ad7d47fe01f73bfec545
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2022-09-02 13:48:44 -0400

    Add problem 1675

--- src/1675/solution.cpp
+++ src/1675/solution.cpp
@@ -1,4 +1,27 @@
 //
-// Created by Islam, Abdullah Al Raqibul on 11/19/21.
+// Created by Islam, Abdullah Al Raqibul on 02/19/22.
 //
 
+class Solution {
+public:
+  int minimumDeviation(vector<int>& nums) {
+    set<int> s;
+    for(auto n: nums) {
+      if(n%2) s.insert(n * 2);
+      else s.insert(n);
+    }
+
+    int ret = INT_MAX, curMax;
+    while(s.size()) {
+      curMax = *s.rbegin();
+      ret = min(ret, curMax - *s.begin());
+
+      if(curMax % 2) break;
+
+      s.erase(curMax);
+      s.insert(curMax/2);
+    }
+
+    return ret;
+  }
+};
